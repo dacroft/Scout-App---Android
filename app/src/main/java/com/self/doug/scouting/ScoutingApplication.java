d9c7357f96dd8c7f10584568c3fd066892559663
Doug Croft <dacroft@gmail.com>
2015-02-26 20:21:50 -0600

    Scouting App - Android Version - Initial Commit
    So far, this app doesn't do much. It is hooked up to use Parse and has
    a somewhat functional login screen.

---
+++ app/src/main/java/com/self/doug/scouting/ScoutingApplication.java
@@ -0,0 +1,29 @@
+package com.self.doug.scouting;
+
+/**
+ * Created by Doug on 2/22/15.
+ */
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseUser;
+
+public class ScoutingApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Allow saving via Parse
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, "8ExTEoeHhV5ANaA9wBHJ8ZAgBpJYIQvbKQ8m4P87", "218c1A4anSh9cAgiyJjBIS8wPfKEUXkF2za754lf");
+
+        // By default, all users are allowed to modify all objects.
+        ParseUser.enableAutomaticUser();
+        ParseACL defaultACL = new ParseACL();
+        defaultACL.setPublicReadAccess(true);
+        ParseACL.setDefaultACL(defaultACL, true);
+    }
+
+}
---
+++ app/src/main/java/com/self/doug/scouting/WelcomeActivity.java
@@ -0,0 +1,69 @@
+package com.self.doug.scouting;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+
+public class WelcomeActivity extends ActionBarActivity {
+
+    // Declare Variable
+    Button logout;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Get the view from singleitemview.xml
+        setContentView(R.layout.activity_welcome);
+/*
+        // Retrieve current user from Parse.com
+        ParseUser currentUser = ParseUser.getCurrentUser();
+
+        // Convert currentUser into String
+        String struser = currentUser.getUsername().toString();
+
+        // Locate TextView in welcome.xml
+        TextView txtuser = (TextView) findViewById(R.id.txtuser);
+
+        // Set the currentUser String into TextView
+        txtuser.setText("You are logged in as " + struser);
+
+        // Locate Button in welcome.xml
+        logout = (Button) findViewById(R.id.logout);
+
+        // Logout Button Click Listener
+        logout.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+                // Logout current user
+                ParseUser.logOut();
+                finish();
+            }
+        });*/
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_welcome, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
